25e65602e67f07909501bdb0820a08d4328118d5
Daniele Cerrina <daniecerri@gmail.com>
2025-11-20 19:01:39 +0100

    Esercizio 1

---
+++ Esercizi/esercizio_base.py
@@ -0,0 +1,8 @@
+"""
+Stai pianificando un viaggio in auto partendo da casa tua a Torino verso una località di mare.
+La destinazione scelta è la città di "Otranto", che dista esattamente 1025 chilometri.
+La tua auto, una vecchia "fiat punto", ha un consumo medio dichiarato di 16.5 chilometri con un litro
+di carburante. Sapendo che il prezzo attuale della benzina è di 1.829 euro al litro e che dovrai
+sostenere una spesa fissa per i pedaggi autostradali pari a 74.50 euro, scrivi uno script che calcoli
+la quantità di litri necessari, il costo del solo carburante e infine il costo totale del viaggio.
+"""
