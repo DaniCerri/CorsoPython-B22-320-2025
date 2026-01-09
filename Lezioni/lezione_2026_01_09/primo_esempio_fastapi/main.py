7be9038d9df166c0011bb5b355cab21790e30a01
Daniele Cerrina <daniecerri@gmail.com>
2026-01-09 19:19:30 +0100

    Primo esempio FastaAPI

---
+++ Lezioni/lezione_2026_01_09/primo_esempio_fastapi/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()  # Istanziamo un oggetto di classe FastAPI che si occuperò di gestire tutta la
+# nostra applicazione server
+
+# Creiamo il dizionario che terrà i nostri dati
+MENU = {
+    1: {"nome": "Carbonara", "prezzo": 14.0},
+    2: {"nome": "Tiramisù", "prezzo": 7.0},
+    3: {"nome": "Tagliata di manzo", "prezzo": 27.50},
+    4: {"nome": "Acqua naturale", "prezzo": 1.50}
+}
+
+# 1.Endpoint base -> ROOT
+@app.get("/")
+def get_info_ristorante():
+    # FastAPI si preoccupa al posto nostro di convertire il dizionario in JSON e di gestire
+    # la coda del server
+    return {
+        "nome": "Ristorante Immaginazione",
+        "luogo": "via Carlo Alberto 22/A",
+        "orari": [
+            {"lun-ven": ["09:00-12:00", "15:00-21:00"]},
+            {"sab-dom": ["11:00-12:00", "18:00-21:00"]}
+        ]
+    }
+
+@app.get("/menu")
+def get_menu():
+    return MENU
+
+@app.get("/menu/{id_piatto}")
+def get_piatto_da_id(id_piatto: int):
+    # FastAPI si preoccuperà di convertire il numero scritto in formato stringa nell'URL al formato
+    # numero int
+
+    # Gestiamo il caso in cui l'id del piatto non è presente
+    if id_piatto not in MENU:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Non esiste un piatto con id {id_piatto}"
+        )
+
+    return MENU[id_piatto]
+
+# Provare a fare una funzione che restituisca solo i piatti che costano meno di un prezzo dato
+"/menu/under/{}"
+
+
+
+
+
+
