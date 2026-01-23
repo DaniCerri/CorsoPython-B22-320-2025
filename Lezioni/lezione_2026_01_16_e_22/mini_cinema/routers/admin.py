9ac206f1185fa386a903e1dffac3f8294fde7f98
Daniele Cerrina <daniecerri@gmail.com>
2026-01-23 16:12:15 +0100

    Checkpoint lezione 23/01/2026

--- Lezioni/lezione_2026_01_16_e_22/mini_cinema/routers/admin.py
+++ Lezioni/lezione_2026_01_16_e_22/mini_cinema/routers/admin.py
@@ -13,8 +13,8 @@ def root():
 def random_seed():
     # 0. Resettiamo cinema_hall
     nuovi_dati = init_cinema()
-    cinema_hall.clear()
-    cinema_hall.extend(nuovi_dati)
+    cinema_hall.clear()  # puliamo la lista ma manteniamo lo stesso indirizzo in memoria
+    cinema_hall.extend(nuovi_dati)  # Aggiungiamo i dati nuovi allo stesso indirizzo in memoria
 
     # 1. trovare il numero (casuale) di posti da prenotare
     n_posti = random.randint(1, rows * cols)
@@ -32,10 +32,6 @@ def random_seed():
         "database": cinema_hall
     }
 
-# TODO: facciamo un endpoint /stats che restituisce le seguenti statistiche:
-#  * riempimento per fila
-#  * riempimento medio della sala (per file)
-#  * lo stesso per colonne
 # 1. fare una funzione che calcoli il conteggio di un insieme di posti
 def conta_prenotati(lista_posti: list[dict]) -> int:
     tot_prenotati = 0
@@ -54,5 +50,45 @@ def media_di_gruppi(gruppi: list[list[dict]]) -> float:
         lista_totali.append(totale_gruppo)
 
     return sum(lista_totali) / len(lista_totali)
+# TODO: fare la media con un solo for (abbastanza implicito - list comprehension)
+
+# 2.5 facciamo una funzione che estragga righe o colonne [della sala] dai dati
+def raggruppa(tipo="colonna"):
+    # Controlliamo che il tipo sia tra quelli disponibili, sennò diamo errore
+    assert tipo in {"colonna", "riga"}, "Il tipo di raggruppamento deve essere 'riga' o 'colonna'"
+    gruppi = {}
+
+    for record in cinema_hall:
+        chiave = record['number' if tipo == "colonna" else 'row']  # Otteniamo il nome della colonna/riga del posto corrente
+        # Cerchiamo di prendere il gruppo con la stessa colonna dal dizionario
+        gruppo = gruppi.get(chiave, [])
+        gruppo.append(record) # Aggiungiamo il posto corrente all'elenco
+        gruppi[chiave] = gruppo  # Sovrascriviamo l'elenco nel dizionario
+
+    return gruppi
 
 # 3. applicare queste funzioni sia alle righe che alle colonne [da ottenere]
+@router.get("/stats")
+def calcola_stats():
+    righe = raggruppa("riga")
+    colonne = raggruppa("colonna")
+
+    media_righe = media_di_gruppi([value for value in righe.values()])
+    media_colonne = media_di_gruppi([value for value in colonne.values()])
+
+    conteggi_righe = {chiave: conta_prenotati(valore) for chiave, valore in righe.items()}
+    conteggi_colonne = {chiave: conta_prenotati(valore) for chiave, valore in colonne.items()}
+    # TODO: Fare una funzione 'aggrega' che data tipologia di raggruppamento faccia i 3 passaggi sopra
+
+    return {
+        "righe": {
+            "media": media_righe,
+            "conteggi": conteggi_righe
+        },
+        "colonne": {
+            "media": media_colonne,
+            "conteggi": conteggi_colonne
+        },
+    }
+
+
