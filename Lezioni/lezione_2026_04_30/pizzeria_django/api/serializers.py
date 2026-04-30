c5ba072d3f2869b073d18cde24ff7b69bc38664c
Daniele Cerrina <daniecerri@gmail.com>
2026-04-30 20:09:21 +0200

    Fine lezione 30/04

---
+++ Lezioni/lezione_2026_04_30/pizzeria_django/api/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import Ingrediente, Pizza, Cliente, Ordine, VocePizzaOrdine
+
+class IngredienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingrediente
+        fields = ['id', 'nome', 'prezzo_extra', 'allergene', 'vegetariano']
+
+class PizzaSerializer(serializers.ModelSerializer):
+    ingredienti = IngredienteSerializer(many=True, read_only=True)
+    ingredienti_ids = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Ingrediente.objects.all(), write_only=True,
+        source='ingredienti', required=False
+    )
+    prezzo_totale = serializers.FloatField(read_only=True)
+
+    class Meta:
+        model = Pizza
+        fields = ['id', 'nome', 'prezzo_base', 'descrizione', 'disponibilita',
+                  'prezzo_totale', 'ingredienti', 'ingredienti_ids']
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = ['id', 'nome', 'telefono', 'indirizzo']
+
+class VoceOrdineCreateSerializer(serializers.ModelSerializer):
+    pizza_id = serializers.IntegerField()
+    quantita = serializers.IntegerField(min_value=1)
+    ingredienti_extra_ids = serializers.ListField(
+        child=serializers.IntegerField(),
+        default=list
+    )
+
+class VoceOrdineSerializer(serializers.ModelSerializer):
+    pizza = PizzaSerializer(read_only=True)
+    ingredienti_extra = IngredienteSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ordine
+        fields = ['id', 'data_ora', 'stato', 'note', 'cliente', 'voci']
+
+
